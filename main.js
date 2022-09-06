ec476f5e630a72b3bb399a5cca19cab54aa80936
derrick <djmulinde17@gmail.com>
2022-09-06 17:15:23 +0300

    Add more javascript to the menu

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const hamburger = document.querySelector('.menu-button');
 const menuClose = document.querySelector('.menu-close');
 const menuOverlay = document.getElementById('menu-overlay');
+const entireOver = document.querySelector('.mobile-menu');
 
 const displayMenuList = () => {
   hamburger.style.display = 'none';
   menuOverlay.style.display = 'block';
+  entireOver.style.display = 'block';
+  menuClose.style.display = 'block';
 };
 
 const closeMenuList = () => {
   menuOverlay.style.display = 'none';
   hamburger.style.display = 'block';
+  entireOver.style.display = 'none';
+  menuClose.style.display = 'none';
 };
 
 hamburger.addEventListener('click', displayMenuList);
@@ -19,5 +24,7 @@ document.querySelectorAll('.menu-item-list').forEach((n) => {
   n.addEventListener('click', () => {
     menuOverlay.style.display = 'none';
     hamburger.style.display = 'block';
+    menuClose.style.display = 'none';
+    entireOver.style.display = 'none';
   });
 });
